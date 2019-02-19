680e24b8ea3da58dc78cf4524a4ee8242491f7ba
Unknown <lsy930611@hotmail.com>
2019-02-19 17:54:21 -0500

    v1.5
    Add first java file and some notes.

---
+++ src/main/main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class main {
+    public static void main(String[] args){
+        connectwBing();
+    }
+
+    public static void connectwBing(){
+        try{
+            URL bing = new URL("https://www.bing.com/");
+            URLConnection connectwithbing = bing.openConnection();
+            BufferedReader res_1=new BufferedReader(new InputStreamReader(connectwithbing.getInputStream()));
+            String res;
+            while((res = res_1.readLine())!= null){
+                System.out.println(res);
+            }
+        }  catch (MalformedURLException e) {
+            System.out.println("Wrong URL");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Connection Failed in first try");
+            e.printStackTrace();
+        }
+
+
+    }
+}
